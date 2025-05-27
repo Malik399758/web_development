1c3319de9ec64d6e8773ee17a9037580c2dec3b4
Abidcomputers <ym399758@gmail.com>
2025-05-27 11:33:22 -0700

    external script.

---
+++ java_script/internal_script/External_script/calculator.js
@@ -0,0 +1,16 @@
+
+function add(a,b){
+    return a+b;
+}
+
+var no1 = prompt('enter first no :','0')
+var no2 = prompt('enter second no :','0')
+
+no1 = parseInt(no1),
+no2 = parseInt(no2)
+
+var add1 = add(no1,no2);
+
+document.write(add1);
+
+
