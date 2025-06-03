f2570869fd99bb57e32ba694c129e8bab2929ee4
Abidcomputers <ym399758@gmail.com>
2025-06-03 12:03:01 -0700

    DOM demo.

--- java_script/internal_script/External_script/calculator.js
+++ java_script/internal_script/External_script/calculator.js
@@ -1,7 +1,13 @@
 
+// add
 function add(a,b){
     return a+b;
 }
+// sub
+function sub(a,b){
+    return a-b;
+}
+
 
 var no1 = prompt('enter first no :','0')
 var no2 = prompt('enter second no :','0')
@@ -10,7 +16,16 @@ no1 = parseInt(no1),
 no2 = parseInt(no2)
 
 var add1 = add(no1,no2);
+var sub1 = sub(no1,no2)
 
-document.write(add1);
+document.write('Addition ',add1);
+document.write('Subtraction ',sub1);
+
+
+
+
+
+
+
 
 
